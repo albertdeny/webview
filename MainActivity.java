b6448e776aa7d81ff4ff1c70560ae476654d5b77
salinas38m <salinas38m@gmail.com>
2019-03-08 21:27:27 -0300

    add files

---
+++ MainActivity.java
@@ -0,0 +1,104 @@
+package salinas38m.definitivewebview;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView)findViewById(R.id.webview);
+
+
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.getSettings().setDatabaseEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        //webView.setWebViewClient(new GeoWebViewClient());
+
+        webView.getSettings().setGeolocationEnabled(true);
+        webView.setWebViewClient(new WebViewClient(){
+
+        });
+
+        //LOAD URL
+        webView.loadUrl("https://www.github.com/salinas38m/webview");
+
+
+        webView.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView webView, int errorCode, String description, String failingUrl) {
+                try {
+                    webView.stopLoading();
+                } catch (Exception e) {
+                }
+
+                //BACK BUTTON
+                if (webView.canGoBack()) {
+                    webView.goBack();
+                }
+
+                //TRY AGAIN ALERT WHEN NO INTERNET CONNECTION (DONT WORK YET)
+                webView.loadUrl("about:blank");
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setTitle("Error");
+                alertDialog.setMessage("No internet connection:(");
+                alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "Try Again", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                        startActivity(getIntent());
+                    }
+                });
+
+                alertDialog.show();
+                super.onReceivedError(webView, errorCode, description, failingUrl);
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient(){
+
+            @Override
+
+            //GEOLOCATION PERMISSIONS. WORKS FOR GOOGLE PLAY!
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                // callback.invoke(String origin, boolean allow, boolean remember);
+                callback.invoke(origin, true, false);
+            }
+
+
+
+
+        });
+
+        //LINKS WHATSAPP SOLVED ERROR UNKNOW URL SCHEME
+        webView.setWebViewClient(new WebViewClient(){
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url != null && (url.startsWith("http://api.whatsapp") || url.startsWith("https://api.whatsapp"))) {
+                    view.getContext().startActivity(
+                            new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
+
+    }
+
+
+}
